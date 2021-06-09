d0cadc7e15bc9654e604365bbae7a4184fdc7eea
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-06-09 17:07:57 +0530

    Side Bar with Nav Link code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import Sidebar from './sidebar'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './App.css'
 
-/**
-* @author
-* @function App
-**/
-
-const App = (props) => {
+const AppSidebar = () => {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div className="" style={{ background: 'rgba(33,150,243,5%)' }}>
+        <Sidebar />
+      </div>
+    </Router>
+  );
 }
 
+class App extends React.Component{
+  render() {
+    return (
+      <AppSidebar />
+    )
+  }
+}
 
-export default App
+export default App;
